68ab1075d43db1b5497e154370a33fb5c7375e0f
Naman-samodrei <namangarg@samodrei.com>
2024-09-18 12:55:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -65,4 +65,4 @@ def verify_email_endpoint():
         return jsonify({'status': 'bad', 'message': 'Invalid email address'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000)
